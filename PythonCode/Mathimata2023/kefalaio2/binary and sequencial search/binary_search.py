bd2be8b6cf516ff0648825ba46ac78d6afee492c
giannisjv <giannisvidras@gmail.com>
2022-11-02 19:18:16 +0200

    binary and sequential serach

---
+++ PythonCode/Mathimata2023/kefalaio2/binary and sequencial search/binary_search.py
@@ -0,0 +1,38 @@
+#binary search over a random number....
+
+import random
+import timeit
+
+start = 1 
+end = 10000
+step = 1
+numForSearch = random.randint(start,end)
+moves_counter = 0
+flag = False
+minNum = start
+maxNum = end
+
+
+start_time = timeit.default_timer() 
+while(flag == False):
+    moves_counter += 1
+    binaryNum = (minNum + maxNum) / 2
+    binaryNum = int(binaryNum)
+
+    if(numForSearch < binaryNum):
+        maxNum = binaryNum - 1
+        print("minNum " ,minNum , " maxNum ", maxNum)
+
+    elif(numForSearch > binaryNum):
+        minNum = binaryNum + 1
+        print("minNum " ,minNum , " maxNum ", maxNum)
+   
+    elif(numForSearch == binaryNum):
+        print("The number was ", numForSearch , "and was found in " , moves_counter , "tri  es")
+        print("The range was between " , start , "to ", end)
+        flag = True
+
+end_time = timeit.default_timer()
+
+execution_time = end_time - start_time
+print("time tooked " ,execution_time)
---
+++ PythonCode/Mathimata2023/kefalaio2/binary and sequencial search/sequential_search.py
@@ -0,0 +1,25 @@
+#sequential search over a random number....
+
+import random
+import timeit
+
+start = 1 
+end = 100
+step = 1
+numForSearch = random.randint(start,end)
+moves_counter = 0
+flag = False
+
+start_time = timeit.default_timer() 
+for i in range(start, end, step):
+    moves_counter += 1
+    if(numForSearch == i):
+        print("The number was ", numForSearch , "and was found in " , moves_counter)
+        print("The range was between " , start , "to ", end)
+        flag = True
+        if(flag == True):
+            break
+end_time = timeit.default_timer()
+
+execution_time = end_time - start_time
+print("time tooked " ,execution_time)
