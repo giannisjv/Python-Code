1b194aee83305eb7e6c8e0a39d04f666e9e394e3
giannisjv <giannisvidras@gmail.com>
2023-01-19 20:07:20 +0200

    Ergasia xmas fixes

--- PythonCode/Mathimata2023/Files/ergasia_xmas.py
+++ PythonCode/Mathimata2023/Files/ergasia_xmas.py
@@ -5,7 +5,7 @@ def IsExisting(path):
 
 anwser = "a"
 while(anwser != "exit"):
-    anwser = input("\n1. Δημιουργία αρχείου\n2. Άνοιγμα αρχείου (Read-only)\n3. Άνοιγμα Αρχείου(Write-Only)\n4. Άνοιγμα αρχείου(Read - Write)\nexit για τέλος: ")
+    anwser = input("\n1. Δημιουργία αρχείου\n2. Άνοιγμα αρχείου (Read-only)\n3. Προσθήκη στο αρχείο\nexit για τέλος: ")
     if (anwser == "1"):
         file_name = input("Δώσε το Όνομα του μαθητή: ")
         path = input("Πού θα ανοιχθεί το αρχείο; ")
@@ -13,10 +13,10 @@ while(anwser != "exit"):
         file_name1 = path + "/" + file
 
 
-        if(IsExisting(file_name) == False):
+        if(IsExisting(file_name1) == False):
             print("Δημιουργία αρχείου", file_name1)
             fileObj = open(file_name1, "w")
-            fileObj.write("Mathima\tVathmos")
+            fileObj.write("Mathima\tVathmos\n")
             insert = "yes" 
 
             while(insert == "yes"):
@@ -25,11 +25,11 @@ while(anwser != "exit"):
                 
                 while(Vathmos < 1 or Vathmos > 20):
                     Vathmos = float(input("Δώσε τον αριθμό από το 1 έως το 20: "))
-                fileObj.write(mathima + "\t" + str(Vathmos))
+                fileObj.write(mathima + "\t" + str(Vathmos) + "\n")
                 insert = input("θέλεις να δώσεις και άλλο μάθημα; yes | no")
             fileObj.close()
 
-        elif(IsExisting(file_name) == True):
+        elif(IsExisting(file_name1) == True):
             print("Το Αρχείο" + file_name1 + "υπάρχει")
             print("Τα δεδομένα ειναι:")
             fileObj = open(file_name1, "r")
@@ -41,3 +41,44 @@ while(anwser != "exit"):
         path = input("Πού θα ανοιχθεί το αρχείο;")
         file = file_name + ".txt"
         file_name1 = path + "/" + file
+        
+        fileObj = open(file_name1,"r")
+        print(fileObj.read())
+        fileObj.close()
+
+    elif(anwser == "3"):
+        file_name = input("Δώσε το Όνομα του μαθητή: ")
+        path = input("Πού θα ανοιχθεί το αρχείο; ")
+        file = file_name + ".txt"
+        file_name1 = path + "/" + file
+
+        if(IsExisting(file_name1) == True):
+            fileObj = open(file_name1, "a")
+            insert = "yes" 
+
+            while(insert == "yes"):
+                mathima = input("Εισαγωγή μαθήματος: ")
+                Vathmos = float(input("Δώσε τον αριθμό από το 1 έως το 20: "))
+                
+                while(Vathmos < 1 or Vathmos > 20):
+                    Vathmos = float(input("Δώσε τον αριθμό από το 1 έως το 20: "))
+                fileObj.write(mathima + "\t" + str(Vathmos) +"\n")
+                insert = input("θέλεις να δώσεις και άλλο μάθημα; yes | no")
+            fileObj.close()
+        elif(IsExisting(file_name1 == False)):
+            fileObj = open(file_name1, "a")
+            fileObj.write("Mathima\tVathmos")
+            insert = "yes" 
+
+            while(insert == "yes"):
+                mathima = input("Εισαγωγή μαθήματος: ")
+                Vathmos = float(input("Δώσε τον αριθμό από το 1 έως το 20: "))
+                
+                while(Vathmos < 1 or Vathmos > 20):
+                    Vathmos = float(input("Δώσε τον αριθμό από το 1 έως το 20: "))
+                fileObj.write(mathima + "\t" + str(Vathmos) + "\n")
+                insert = input("θέλεις να δώσεις και άλλο μάθημα; yes | no")
+            fileObj.close()
+
+        else:
+            print("Wrong anwser you little redneck")
