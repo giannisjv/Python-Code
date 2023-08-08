79893db21980dcc2ca3c07449972636bd431248f
giannisjv <giannisvidras@gmail.com>
2023-08-08 17:10:25 +0300

    κδφλσεξκφ

--- PythonCode/MyCars.py
+++ PythonCode/MyCars.py
@@ -11,7 +11,14 @@ class  mileage(car):
     def __init__(self, brand, model, year_of_production, service):
         super().__init__(brand, model, year_of_production)
         self.service = service
+    def welcome(self):
+        print("Your  have to service your car", self.brand, self.model, "at", self.service, "Km")
+        print("Your car is ", 2023 - self.year_of_production, "Years old")
 
-Car1 = mileage("Reault", "Megane", 2004)
-Car1.service = 2444639
+class voltage(mileage):
+    pass
+
+Car1 = mileage("Reault", "Megane", 2004, 244456)
+#print(Car1.service)
+Car1.welcome()
 print(Car1)
